2f3d23c51a9728490ec6731230700387f530f190
Tanweer Kamal <tanweerkamal54@gmail.com>
2024-04-03 13:40:51 +0530

    Changed status id to dynamic id of user

--- src/pages/user/UserIndexPage.vue
+++ src/pages/user/UserIndexPage.vue
@@ -116,7 +116,7 @@
                                         <div class="flex flex-row">
                                             <ul class="py-1 text-sm text-gray-700 dark:text-gray-200 flex flex-row" aria-labelledby="apple-imac-27-dropdown-button">
                                                 <li>
-                                                    <router-link :to="{name: 'user.show', params: {id: 123}}" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                                    <router-link :to="{name: 'user.show', params: {id: u.id}}" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                                                         <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                                         <path stroke="currentColor" stroke-width="2" d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z"/>
                                                         <path stroke="currentColor" stroke-width="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
