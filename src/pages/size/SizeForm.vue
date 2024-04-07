dfdc57373d89b13c182b4710f4e4b7a841c75468
Tanweer Kamal <tanweerkamal54@gmail.com>
2024-04-07 15:05:53 +0530

    Created SizeForm component

---
+++ src/pages/size/SizeForm.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="relative p-4 w-full max-w-xl h-auto md:h-auto mx-auto">
+        <!-- Modal content -->
+        <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+            <!-- Modal header -->
+            <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                    Edit Size
+                </h3>
+            </div>
+            <!-- Modal body -->
+            <form action="#" @submit.prevent="$emit('add-size')">
+                <div class="grid gap-4 mb-4 sm:grid-cols-2">
+                    <div>
+                        <label for="size" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Size</label>
+                        <input type="text" v-model="sizeModel.size" name="size" id="size" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="e.g, XS, S, M, L, XL, etc" required="">
+                    </div>
+                </div>
+                <button type="submit" class="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                    <svg class="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+                    Save
+                </button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        sizeModel: Object
+    }
+}
+</script>
