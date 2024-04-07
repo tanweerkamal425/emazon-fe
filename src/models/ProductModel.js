39c36b779f94149407ccc319684a73ad1dc0c49a
Tanweer Kamal <tanweerkamal54@gmail.com>
2024-04-07 13:38:47 +0530

    Added ProductModel and ProductForm component

---
+++ src/models/ProductModel.js
@@ -0,0 +1,9 @@
+export let ProductModel = {
+    code: "",
+    title: "",
+    subtitle: "",
+    price_sp: null,
+    price_mp: null,
+    category_id: null,
+    description: ""
+}
---
+++ src/pages/product/ProductForm.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="relative p-4 w-full max-w-xl h-full md:h-auto mx-auto">
+        <!-- Modal content -->
+        <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+            <!-- Modal header -->
+            <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                    {{ name }}
+                </h3>
+            </div>
+            <!-- Modal body -->
+            <form action="#" @submit.prevent="$emit('add-product')">
+                <div class="grid gap-4 mb-4 sm:grid-cols-2">
+                    <div>
+                        <label for="code" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Code</label>
+                        <!-- {{ productModel.code }} -->
+                        <input type="text" name="code" id="code" v-model="productModel.code" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product code" required="">
+                    </div>
+                    <div>
+                        <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+                        <!-- {{ productModel.title }} -->
+                        <input type="text" name="title" id="title" v-model="productModel.title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product title" required="">
+                    </div>
+                    <div>
+                        <label for="subtitle" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Subitle</label>
+                        <input type="text" name="subtitle" v-model="productModel.subtitle" id="subtitle" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Product subtitle" required="">
+                    </div>
+                    <div>
+                        <label for="price_mp" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Marked Price</label>
+                        <input type="number" v-model="productModel.price_mp" name="price_mp" id="price_mp" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required="">
+                    </div>
+                    <div>
+                        <label for="price_sp" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Selling Price</label>
+                        <input type="number" v-model="productModel.price_sp" name="price_sp" id="price_sp" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required="">
+                    </div>
+                    <div>
+                        <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                        <select v-model="productModel.category_id" id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            <option selected="">Select category</option>
+                            <template v-for="c in categories" :key="c.id">
+                                <option :value="c.id">{{ c.name }}</option>
+                            </template>
+                        </select>
+                    </div>
+                    <div>
+                        <label for="image_url" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image</label>
+                        <input type="file" name="image_url" id="image_url" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="enter image url">
+                    </div>
+                    <div class="sm:col-span-2">
+                        <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                        <textarea id="description" v-model="productModel.description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Write product description here"></textarea>                    
+                    </div>
+                </div>
+                <button type="submit" class="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                    <svg class="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+                    Add new product
+                </button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        productModel: Object,
+        categories: Array,
+        name: String
+    },
+
+    data() {
+        return {
+            selectedId: null,
+        }
+    }
+}
+</script>
