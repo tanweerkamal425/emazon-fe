1f824bbca02066db35a6f928bac5a771ca801845
Tanweer Kamal <tanweerkamal54@gmail.com>
2024-04-07 13:39:29 +0530

    Created SuccessToast component

---
+++ src/components/flowbite/SuccessToast.vue
@@ -0,0 +1,9 @@
+<template>
+    <fwb-toast closable type="success">
+        Item moved successfully.
+    </fwb-toast>
+</template>
+
+<script setup>
+import { FwbToast } from 'flowbite-vue'
+</script>
