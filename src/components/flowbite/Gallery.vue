0cdd41606a365ec4f224023a014653894b5c4ea3
Tanweer Kamal <tanweerkamal54@gmail.com>
2024-04-08 22:07:45 +0530

    Created Gallery and ProductImageForm component

---
+++ src/components/flowbite/Gallery.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="grid grid-cols-2 md:grid-cols-6 gap-4">
+    <div v-for="image in imageArr" :key="image.id">
+        <img class="h-auto max-w-full rounded-lg" :src="image.image" alt="">
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+    props: {
+        imageArr: Array
+    }
+}
+</script>
---
+++ src/pages/product/ProductImageForm.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="relative p-4 w-full max-w-xl h-auto md:h-auto">
+
+        <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+
+            <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                    Add Image
+                </h3>
+                <button @click="$emit('close-form')" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <form @submit.prevent="$emit('upload-image')" action="#">
+                <div class="grid gap-4 mb-4 sm:grid-cols-1">
+                    <div>
+                        <label for="image_url" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image</label>
+                        <input @change="onFileSelect" type="file" name="image_url" id="image_url" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter image URL" required="">
+                    </div>
+                    <div>
+                        <label for="is_default" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Default</label>
+                        <select id="is_default" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            <option selected value="1">1</option>
+                            <option value="0">0</option>
+                        </select>
+                    </div>
+                </div>
+                <button type="submit" class="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                    <svg class="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+                    Add new image
+                </button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        onFileSelect(event) {
+            const file = event.target.files[0];
+            this.$emit('file-select', file);
+        }
+    }
+}
+</script>
