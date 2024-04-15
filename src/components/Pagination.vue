07613c7b37c9de90880f6dec1701ae54744af46d
Tanweer Kamal <tanweerkamal54@gmail.com>
2024-04-15 13:20:44 +0530

    Created pagination and select component

---
+++ src/components/Pagination.vue
@@ -0,0 +1,21 @@
+<template>
+    <fwb-pagination @click="$emit('page-change', currentPage)" v-model="currentPage" :total-items="100"></fwb-pagination>
+    <!-- <fwb-pagination v-model="currentPage" :total-items="100" large></fwb-pagination> -->
+</template>
+
+
+<script>
+import { FwbPagination } from 'flowbite-vue'
+
+export default {
+    components: {
+        FwbPagination
+    },
+
+    data() {
+        return {
+            currentPage: 1
+        }
+    }
+}
+</script>
---
+++ src/components/flowbite/Select.vue
@@ -0,0 +1,31 @@
+<template>
+<fwb-select
+    v-model="selected"
+    :options="countries"
+    label="Select a country"
+/>
+</template>
+
+<script>
+import { FwbSelect } from 'flowbite-vue'
+
+export default {
+    components: {
+        FwbSelect,
+    },
+
+    data() {
+        return {
+            selected: '',
+            // countries : [
+            //     { value: 'us', name: 'United States' },
+            //     { value: 'ca', name: 'Canada' },
+            //     { value: 'fr', name: 'France' },
+            // ]
+        }
+    },
+
+
+}
+
+</script>
