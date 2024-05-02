537bd82dc467a22f5a89f5b5ac4cf08af840967e
Tanweer Kamal <tanweerkamal54@gmail.com>
2024-05-02 13:11:32 +0530

    Added searchInput, value-wise sort and active-inactive filter in coupon index page

--- src/pages/coupon/CouponIndexPage.vue
+++ src/pages/coupon/CouponIndexPage.vue
@@ -14,7 +14,7 @@
                                         <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
                                     </svg>
                                 </div>
-                                <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search" required="">
+                                <input @input="onInput" name=search type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search" required="">
                             </div>
                         </form>
                     </div>
@@ -70,7 +70,18 @@
                                     </li>
                                 </ul>
                             </div>
+                            <select @input="onSelected" id="value" name="value" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option value="" selected>Sort value</option>
+                                <option value="1">Low to high</option>
+                                <option value="2">High to low</option>
+                            </select>
+                            <select @input="onSelected" id="isActive" name="isActive" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option value="" selected>Choose one</option>
+                                <option value="1">Inactive</option>
+                                <option value="2">Active</option>
+                            </select>
                         </div>
+
                         <router-link :to="{name: 'coupon.store'}" type="button" class="flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
                             <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                 <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
@@ -98,7 +109,7 @@
                                     <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ c.id }}</th>
                                     <td class="px-4 py-3">{{c.coupon}}</td>
                                     <td class="px-4 py-3">{{ c.value }}%</td>
-                                    <td class="px-4 py-3">{{c.active}}</td>
+                                    <td class="px-4 py-3">{{c.is_active == 1 ? 'Active' : 'Inactive'}}</td>
                                     <td class="px-4 py-3 flex items-center justify-end">
                                         <!-- <button id="apple-imac-27-dropdown-button" data-dropdown-toggle="apple-imac-27-dropdown" class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
                                             <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -170,8 +181,14 @@ export default {
     data() {
         return {
             coupons: [],
+            searchInput: '',
+            value: '',
+            isActive: '',
             query: {
-                page: ''
+                page: '',
+                coupon: '',
+                value: '',
+                is_active: '',
             }
         }
     },
@@ -191,6 +208,19 @@ export default {
         onPageChange(pageNum) {
             this.query.page = pageNum;
             this.fetchCoupons(this.query);
+        },
+
+        onInput(event) {
+            this.searchInput = event.target.value;
+        },
+
+        onSelected(event) {
+            if (event.target.id == 'value') {
+                this.value = event.target.value;
+            }
+            if (event.target.id == 'isActive') {
+                this.isActive = event.target.value;
+            }
         }
     },
 
@@ -201,6 +231,23 @@ export default {
 
     computed: {
         ...mapState(useCouponStore, ["getCoupons"]),
+    },
+
+    watch: {
+        searchInput() {
+            this.query.coupon = this.searchInput != null ? this.searchInput : '';
+            this.fetchCoupons(this.query);
+        },
+
+        value() {
+            this.query.value = this.value;
+            this.fetchCoupons(this.query);
+        },
+
+        isActive() {
+            this.query.is_active = this.isActive;
+            this.fetchCoupons(this.query);
+        }
     }
 }
 
